0f80fc36c1fdb1e60ce345f3f338259fb424e5e5
Leroy <leroypawlick@gmail.com>
2023-02-04 16:14:08 -0700

    added extra credit

---
+++ extracredit.js
@@ -0,0 +1,8 @@
+// 1) Insomniac Games
+// - per job postings for lead programmers, Insomniac requires profiency in either Javascript, Perl, or Python as a scripting language. Also need programmers that use C and C++
+
+// 2) Marvel Entertainment
+// - job listing for back-end dev shows need for knowledge in Java, as well as, C# and jQuery
+
+// 3) Spotify
+// - a job listing for Web Dev shows need for strong skills in core JavaScript, CSS, HTML5. Also prefers devs working in the agile processes
